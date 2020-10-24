f3f6beef1155a88bd648b916e6e1df08415f3d24
Abdou <abdou.g12dz@gmail.com>
2020-10-24 17:05:00 +0100

    New Feature: search autocomplete

--- index.js
+++ index.js
@@ -6,40 +6,65 @@ const search = async (query, type, pageToken = null) => {
     const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
         params: {
             key: APIKEY,
-            q: query,
+            q: query.trim(),
             type: type,
             part: 'snippet',
             pageToken: pageToken
         }
     });
-    console.log(response.data);
+    return response.data.items;
 }
 
-/* const fetchVideoData =  async (id) => {
-    const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
-        params:{ 
-            key: APIKEY,
-            part: 'statistics, snippet',
-            id: id,
-           // fields: 'items(statistics)'
-        }
-    });
-    console.log(response.data);
-}
-
-// W6NZfCO5SIk */
+/** HTML GENERATION */
+const searchContainer = document.querySelector('#search');
+searchContainer.innerHTML = `
+    <div class="dropdown">
+        <input id="search_kw" type="text" name="kw" class="input" placeholder="Type something here...">
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+               
+            </div>
+        </div>
+    </div>
+    <div class="select">
+        <select name="type" id="type">
+            <option value="video">Video</option>
+            <option value="channel">Channel</option>
+            <option value="playlist">Playlist</option>
+        </select>
+    </div>
+`;
 
 const type = document.querySelector('#type');
 const searchKw = document.querySelector('#search_kw');
+const autocompleteDropdown = searchContainer.querySelector('.dropdown');
+const autocompleteContent= searchContainer.querySelector('.dropdown-content');
 
 
-const onInput = e => {
-    search(e.target.value, type.value);
+const onInput = async e => {
+    const items = await search(e.target.value, type.value);
+
+    //make sure to delete previous results for the new search
+    autocompleteContent.innerHTML = ``;
+
+    autocompleteDropdown.classList.add('is-active');
+    for (const item of items) {
+        const result = document.createElement('a');
+        result.classList.add('dropdown-item');
+        result.innerHTML = `
+        
+                <img src="${item.snippet.thumbnails.default.url}" />
+                <h3>${item.snippet.title}</h3>
+
+        `;
+        autocompleteContent.appendChild(result);
+    }
 }
 
-const onTypeChange = e => {
+const onTypeChange = async e => {
     if(searchKw.value.trim().length > 0 ){
-        search(e.target.value, type.value);
+        const items = await search(e.target.value, type.value);
+        console.log(items);
     }
 }
 
